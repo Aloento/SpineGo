dd21ea30c1ee9836cfa220736c7131b9714ee09d
Aloento <11802769+soarteam@users.noreply.github.com>
2021-02-13 20:30:16 +0100

    Go

--- src/SpineGo/SkeletonBinary.go
+++ src/SpineGo/SkeletonBinary.go
@@ -2,17 +2,51 @@ package SpineGo
 
 import (
 	"bufio"
+	"log"
 	"os"
 )
+
+var byteSlice = make([]byte, 512)
 
 type skeletonInput struct {
 	bufio.Reader
 	Strings []string
-	chars   []rune
+	chars   [32]rune
 }
 
-func (input *skeletonInput) skeletonInput(file *os.File) {
-	input.Reader = *bufio.NewReader(file)
+func (i *skeletonInput) skeletonInput(file *os.File) {
+	i.Reader = *bufio.NewReader(file)
+}
+
+func (i skeletonInput) readInt(optimizePositive bool) (result int) {
+	b, err := i.ReadByte()
+	result = int(b) & 0x7F
+	if (b & 0x80) != 0 {
+		b, err = i.ReadByte()
+		result |= int(b&0x7F) << 14
+		if (b & 0x80) != 0 {
+			b, err = i.ReadByte()
+			result |= int(b&0x7F) << 21
+			if (b & 0x80) != 0 {
+				b, err = i.ReadByte()
+				result |= int(b&0x7F) << 28
+			}
+		}
+	}
+	if err != nil {
+		log.Println("ReadByte Error:", err)
+		return -1
+	} else {
+		if optimizePositive {
+			return
+		} else {
+			return int(uint(result)>>1) ^ -(result & 1)
+		}
+	}
+}
+
+func (i skeletonInput) readStringRef() string {
+	return ""
 
 }
 
